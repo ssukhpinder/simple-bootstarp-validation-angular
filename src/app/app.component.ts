5b08ca51f447faaab5292661d79f4017ea1dc188
Sukhpinder Singh <sukhpinder.singh@sourcefuse.com>
2020-07-31 01:19:44 +0530

    First Checkin

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'simple-bootstarp-validation-angular';
+
+  validate(){
+    var form = document.getElementsByClassName('needs-validation')[0] as HTMLFormElement;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+    form.classList.add('was-validated');
+  }
 }
